ac4ba131b6f3969b094a277cb2fa2baee7c37552
MatthNig <matthias.niggli@unibas.ch>
2020-10-16 17:48:52 +0200

    init repo structure

--- Code/model_training/LSTM_training.R
+++ Code/model_training/LSTM_training.R
@@ -4,3 +4,194 @@
 # Authors:        Matthias Niggli/CIEB UniBasel                 #
 # Date:           16.10.2020                                    #
 #################################################################
+
+#######################################
+## Load packages and set directories ##
+#######################################
+
+# packages for data processing: ------------------------------------------------
+library("tidyverse")
+library("stringi")
+
+# packages for training the network: -------------------------------------------
+library("tensorflow")
+library("keras")
+library("reticulate")
+
+# directories  -----------------------------------------------------------------
+mainDir1 <- "/scicore/home/weder/GROUP/Innovation/01_patent_data"
+if(substr(getwd(), nchar(getwd())-23, nchar(getwd())) != "/innoscape-data-creation"){
+        print("make sure your working directory is the GitHub repository. Please specify with setwd().")}else{
+                print("Directories are set and packages are loaded")}
+
+################################################
+######### split to train and test set ##########
+################################################
+
+train_frac <- 0.8 # fraction of data to train the model
+N <- nrow(df_train)
+set.seed(10082020)
+train_idx <- sample(seq(N), N * train_frac)
+
+x_train <- x_dat[train_idx, , ]
+x_val <- x_dat[-train_idx, , ]
+y_train <- y_dat[train_idx, ]
+y_val <- y_dat[-train_idx, ]
+
+#########################################
+############ train the network ##########
+#########################################
+
+## build and compile the model -------------------------------------------------
+model <- keras_model_sequential()
+model %>%
+        layer_lstm(units = 512, return_sequences = TRUE, # original: 512 hidden units
+                   input_shape = c(ncol(x_train), dim(x_train)[3])) %>%
+        layer_dropout(rate = 0.33) %>% # original: 0.2
+        layer_lstm(units = 256, return_sequences = TRUE) %>% # original: 256 hidden units
+        layer_dropout(rate = 0.33) %>% # original: 0.2
+        layer_lstm(units = 64) %>% # original: 64 hidden units
+        layer_dropout(rate = 0.1) %>% # original: 0.1
+        layer_dense(units = ncol(y_train), activation = "softmax")
+summary(model)
+
+model %>% compile(
+        optimizer =  "adam",
+        loss = "categorical_crossentropy",
+        metrics = c("accuracy"))
+
+## class_weights: --------------------------------------------------------------
+y_classes$class_weights <- 1
+y_classes$class_weights <- ifelse(y_classes$levels %in% c("Turkey", "Persion", "MiddleEast", "SouthEastAsia"),
+                                  10, 1)
+# y_classes$class_weights <- ifelse(y_classes$levels %in% c("AngloSaxon", "German"), 10, 1)
+y_classes[y_classes$levels == "AngloSaxon", "class_weights"] <- 5
+# y_classes[y_classes$levels == "German", "class_weights" ] <- 10
+CLASS_WEIGHTS <- as.list(y_classes$class_weights)
+names(CLASS_WEIGHTS) <- y_classes$numbers
+
+## fit the model ---------------------------------------------------------------
+EPOCHS <- 20
+BATCH_SIZE <- 256 # original = 128
+
+hist <- model %>% fit(
+        x = x_train, y = y_train, 
+        class_weights = CLASS_WEIGHTS,
+        validation_data = list(x_val, y_val),
+        callbacks = list(callback_early_stopping(monitor = "val_loss", 
+                                                 patience = 3, 
+                                                 restore_best_weights = TRUE)),
+        epochs = EPOCHS, batch_size = BATCH_SIZE, verbose = 2)
+
+# Results: ---------------------------------------------------------------------
+# => 80.01% accuracy with 10 epochs (RUS/EE & China/SEA combined, maximum sample i.e. 32k)
+# => 79.23% accuracy with 10 epochs (RUS/EE & China/SEA combined, maximum sample i.e. 32k + class_weights = both 5)
+# => 83.33% accuracy with 10 epochs (RUS/EE & China/SEA combined, maximum sample i.e. 32k + class_weights = both 10) => overfitting and little learning
+# => 78.61% accuracy with 10 epochs (RUS/EE & China/SEA combined, maximum sample i.e. 32k + class_weights Germany only = 15 & reduced model complexity)
+# => 79.64% accuracy with 10 epochs (RUS/EE & China/SEA combined, maximum sample i.e. 32k + class_weights 7.5/15)
+# => 78.33% accuracy with 10 epochs (RUS/EE & China/SEA combined, maximum sample i.e. 32k + class_weights both 7.5)
+# => 82.93% accuracy with 10 epochs (RUS/EE & China/SEA combined, maximum sample i.e. 32k + class_weights Germany only = 10) => overfitting and some learning
+# => 75.13% accuracy with 10 epochs (RUS/EE & China/SEA combined, enhanced sample + class_weights Germany only = 10, BATCH_SIZE = 256)
+# => 81.60% accuracy with 10 epochs (RUS/EE & China/SEA combined, enhanced sample, no weights, BATCH_SIZE = 256)
+
+# => 81.87% accuracy with 20 epochs (RUS/EE & China/SEA combined, full sample, no weights, BATCH = 256): all classes with minimum recall/precision of 75% 
+# => 82.51% accuracy with 20 epochs (RUS/EE & China/SEA combined, full sample, no weights, BATCH = 256, dropout = 0.33): recall range is slightly larger than before
+
+## IDEA
+# The goal is that recall/precision is similar for all classes. then we have no bias when calculating shares
+# => Thus, increase class weights for those classes where recall is lower than average
+
+## EXTENSIONS  ----------------------------------
+
+## (1) results with China & SE-Asia separated:
+# => 81.70% accuracy with 20 epochs (full sample, no weights, BATCH = 256, dropout = 0.33(0.1 at last stage))
+#    Model would maybe even learn more with more than 20 eepochs
+#    China: 84.19% f1, SEAsia: 37.62% f1, JPN: 88.16% f1, KOR: 88.78% f1
+#    PERFORMANCES DO NOT DECREASE FOR OTHER ASIAN COUNTRIES BUT VERY LOW FOR SE-ASIA. This does not make sense
+
+## (2) results with Persia & Turkey separated:
+# => 80.99% accuracy with 20 epochs (full sample, BATCH = 256, dropout = 0.33(0.1 at last stage))
+#    MiddleEast: 64.8% f1, Turkey: 76.05% f1, Persia: 70.47% f1
+#    AngloSaxon: f1: 0.7867784 acc: 0.6485035
+
+## (3) results with all of them separated (weights = 10) AND weights for AngloSaxon (weight = 5)
+# => 80.62% accuracy with 20 epochs (full sample, weight = 5 for Turkey, MiddleEast, Persia & SEASia, 
+#                                     BATCH = 256, dropout = 0.33(0.1 at last stage))
+# => Model would maybe even learn more with more than 20 epochs
+#    China: xxxx acc, SEAsia: xxxx acc, JPN: xxxx acc, KOR: xxxx acc
+#    MiddleEast: xxxx acc, Turkey: xxxx acc, Persia: xxxx acc 
+#    AngloSaxon: f1: xxxx acc: xxxx
+
+#########################################
+############ evaluate the model #########
+#########################################
+
+tmp <- df_train[-train_idx, ]
+tmp$pred <- as.numeric(model %>% predict_classes(x_val[, ,]))
+tmp$pred <- y_classes[tmp$pred + 1, "levels"]
+tmp$res <- tmp$origin == tmp$pred
+
+## overall accuracy: -----------------------------------------------------------
+table(tmp$res)/nrow(tmp)
+
+# confusion matrix by origin -----------------------------------------------------------
+conf_matrix_fun <- function(region){
+        tmp <- tmp %>% filter(origin == region | pred == region)
+        tmp <- tmp %>% mutate(origin = ifelse(origin == region, 1, 0),
+                              pred = ifelse(pred == region, 1, 0))
+        tmp <- table(tmp$origin, tmp$pred)
+        return(tmp)
+}
+conf_matrix_fun("AngloSaxon")
+
+## precision by origin: --------------------------------------------------------
+# => i.e. "how many of the predicted class are also from this class?" (TP / TP + FP)
+origin_precision <- tmp %>% group_by(pred) %>% summarise(n_obs = n(), 
+                                                         origin_precision = sum(res == TRUE) / n()) %>%
+        rename(region = pred) %>%
+        arrange(origin_precision)
+origin_precision %>% arrange(origin_precision)
+
+## recall by origin: -----------------------------------------------------------
+# "how many form one classes are predicted in this class?" (TP / TP + FN)
+origin_recall <- tmp %>% group_by(origin) %>% summarise(n_obs = n(), 
+                                                        origin_recall = sum(res == TRUE) / n()) %>%
+        rename(region = origin) %>%
+        arrange(origin_recall)
+origin_recall %>% arrange(origin_recall)
+
+## F1 by origin ----------------------------------------------------------------
+origin_eval <- merge(origin_precision[, c("region", "origin_precision")],
+                     origin_recall[, c("region", "origin_recall")], by = "region")
+origin_eval$f1 <- 2 * (origin_eval$origin_precision * origin_eval$origin_recall) / 
+        (origin_eval$origin_precision + origin_eval$origin_recall)
+origin_eval %>% arrange(f1)
+
+# accuracy of all origin classes ------------------------------------------
+acc_fun <- function(ctry){
+        conf_matrix <- conf_matrix_fun(ctry)
+        acc <- sum(diag(conf_matrix)) / sum(conf_matrix)
+        return(acc)
+}
+origin_eval$accuracy <- unlist(lapply(origin_eval$region, acc_fun))
+origin_eval %>% arrange(accuracy)
+
+## median values and range of values across classes:
+sapply(origin_eval[,-1], median)
+sapply(origin_eval[,-1], range)
+
+
+# Remarks -------------------------------------------
+# weighting seems to make things worse because it always makes precision increasing
+# => challenge is that German is relatively bad. This is a problem because of our focus on CH
+# => maybe also weight French
+
+## To-Do's----------------------------------------------
+## 1) weight initialization
+## 2) higher drop-out to prevent overfitting but be careful
+## 3) larger batch size to ensure that there are a few labels of each class per batch
+## 4) add early stopping & weight decay
+## 5) maybe make a smaller model
+## 6) maybe make class_weights proportional to share? e.g. weight_i = (1 / N_i)*N/2 
+## https://www.tensorflow.org/tutorials/structured_data/imbalanced_data
+## https://karpathy.github.io/2019/04/25/recipe/#2-set-up-the-end-to-end-trainingevaluation-skeleton--get-dumb-baselines
--- Code/training_data/train_dat_encoding.R
+++ Code/training_data/train_dat_encoding.R
@@ -4,3 +4,272 @@
 # Authors:        Matthias Niggli/CIEB UniBasel                 #
 # Date:           16.10.2020                                    #
 #################################################################
+
+#######################################
+## Load packages and set directories ##
+#######################################
+
+# packages for data processing: ------------------------------------------------
+library("tidyverse")
+library("stringi")
+
+# directories  -----------------------------------------------------------------
+mainDir1 <- "/scicore/home/weder/GROUP/Innovation/01_patent_data"
+if(substr(getwd(), nchar(getwd())-23, nchar(getwd())) != "/innoscape-data-creation"){
+        print("make sure your working directory is the GitHub repository. Please specify with setwd().")}else{
+                print("Directories are set and packages are loaded")}
+
+# function to encode names -----------------------------------------------------
+source(paste0(getwd(),"/inv_gender/names_encoding_function.R"))
+print("Functions are loaded")
+
+###################################
+############ Load data ############
+###################################
+
+df <- readRDS(paste0(mainDir1, "/created data/origin_training_sample.rds"))
+print("Data is loaded")
+
+######################################################
+############ Create custom country groups ############
+######################################################
+
+name_prism_origin <- names(df)[3:41]
+ref_list <- data.frame(name_prism_origin = name_prism_origin,
+                       origin_group = NA)
+
+ref_list[grep("Africa", ref_list$name_prism_origin), 
+         "origin_group"] <- "Africa"
+ref_list[grep("Europe", ref_list$name_prism_origin), 
+         "origin_group"] <- "EastEurope"
+ref_list[grep("Rus", ref_list$name_prism_origin), 
+         "origin_group"] <- "Russian"
+ref_list[grep("German", ref_list$name_prism_origin), 
+         "origin_group"] <- "German"
+ref_list[grep("Ital", ref_list$name_prism_origin), 
+         "origin_group"] <- "Italian"
+ref_list[grep("Fren", ref_list$name_prism_origin), 
+         "origin_group"] <- "French"
+ref_list[grep("Celtic", ref_list$name_prism_origin), 
+         "origin_group"] <- "AngloSaxon"
+ref_list[grep("Nordic", ref_list$name_prism_origin), 
+         "origin_group"] <- "Scandinavian"
+ref_list[grep("Muslim", ref_list$name_prism_origin), 
+         "origin_group"] <- "MiddleEast"
+
+ref_list[grep("Persian", ref_list$name_prism_origin), 
+         "origin_group"] <- "Persian"
+ref_list[grep("Turkey", ref_list$name_prism_origin), 
+         "origin_group"] <- "Turkey"
+
+ref_list[grep("EastAsia", ref_list$name_prism_origin), 
+         "origin_group"] <- "SouthEastAsia"
+ref_list[grep("Chin", ref_list$name_prism_origin), 
+         "origin_group"] <- "China"
+ref_list[grep("Jap", ref_list$name_prism_origin), 
+         "origin_group"] <- "Japan"
+ref_list[grep("Kore", ref_list$name_prism_origin), 
+         "origin_group"] <- "Korea"
+ref_list[grep("Hisp", ref_list$name_prism_origin), 
+         "origin_group"] <- "HispanicLatinAmerica"
+ref_list[grep("Phil", ref_list$name_prism_origin), 
+         "origin_group"] <- "Philippines"
+ref_list[grep("SouthAsia", ref_list$name_prism_origin), 
+         "origin_group"] <- "India"
+ref_list[grep("Jewish", ref_list$name_prism_origin), 
+         "origin_group"] <- "Jewish"
+ref_list[grep("Greek", ref_list$name_prism_origin), 
+         "origin_group"] <- "Greek"
+
+# re-group to larger regions
+ref_list[ref_list$origin_group %in% c("Russian", "EastEurope"), "origin_group"] <- "Russian&EastEurope"
+#ref_list[ref_list$origin_group %in% c("China", "SouthEastAsia"), "origin_group"] <- "China&SouthEastAsia"
+
+# prepare for classification
+N = nrow(df)
+n_X <- length(unique(ref_list$origin_group))+1
+origin_groups <- as.data.frame(matrix(rep(NA, N * n_X), nrow = N))
+colnames(origin_groups) <- c(colnames(df)[1], 
+                             unique(ref_list$origin_group))
+origin_groups[, 1] <- df[, 1]
+
+## sum the corresponding probabilities together --------------------------------
+for(i in 2:ncol(origin_groups)){
+        origin_group <- colnames(origin_groups)[i]
+        origins <- ref_list[ref_list$origin_group == origin_group, "name_prism_origin"]
+        
+        if(is.data.frame(df[, origins]) == FALSE){
+                origin_prob <- df[, origins]}else{
+                        origin_prob <- rowSums(df[, origins])}
+        
+        origin_groups[, origin_group] <- origin_prob
+}
+
+df <- origin_groups
+paste0("Custom class probabilities calculated for ", nrow(df), " observations")
+
+#########################################################
+############# Evaluate, subset and classify #############
+#########################################################
+
+## get maximum prediction
+df$max_pred <- sapply(seq(1, nrow(df)), function(i){
+        max_pred <- as.numeric(df[i, 2:ncol(df)])
+        max_pred <- max(max_pred, na.rm = TRUE)
+        return(max_pred)
+}
+)
+
+## calculate distance to the second highest prediction
+df$distance_2nd <- sapply(seq(1, nrow(df)), function(i){
+        distance_2nd <- as.numeric(df[i, 2:(ncol(df)-1)])
+        distance_2nd <- sort(distance_2nd, decreasing = TRUE)[1:2]
+        distance_2nd <- abs(diff(distance_2nd))
+        return(distance_2nd)
+}
+)
+
+## calculate entropy
+df$entropy <- sapply(seq(1, nrow(df)), function(i){
+        entropy <- as.numeric(df[i, 2:(ncol(df)-2)])
+        entropy <- entropy * log(entropy)
+        entropy <- -sum(entropy)
+        return(entropy)
+}
+)
+
+## subset to clearly identified names: -----------------------------------------
+# HERE I COULD ALSO TRY OUT DIFFERENT COMBINATIONS OF PARAMETERS AND THEN ESTIMATE THE MODELS WITH
+# THESE DIFFERNET DATASETS. USE THE DATASET WITH THE BEST CLASSIFICATION PERFORMANCE
+
+min_pred <- 0.6
+min_dist <- 0.2
+max_entropy <- 2
+
+df <- df %>% filter(max_pred >= min_pred,
+                    distance_2nd >= min_dist,
+                    entropy <= max_entropy)
+
+paste0("Sample cleaned from ambigous origin classifications. Loosing ", 
+       100 * (1-(nrow(df)/N)), "% of oberservations")
+
+## classify "origin" to the maximum value --------------------------------------
+df$origin <- sapply(seq(1, nrow(df)), function(i){
+        tmp <- as.numeric(df[i, 2:(ncol(df)-3)])
+        max_origin <- which(tmp == df[i, "max_pred"])
+        origin <- gsub("," ,"_", names(df)[1+max_origin])
+        return(origin)
+}
+)
+
+paste0("Training data ready. ", nrow(df),
+       " names classified to the highest origin probability")
+
+############################################
+############ BALANCE THE SAMPLE ############
+############################################
+
+## Drop, up-/downsample classes in the training data --------------------------------------------
+df <- select(df, full_name, origin)
+df %>% group_by(origin) %>% summarise(count = n())
+
+set.seed(10082020)
+df_train <- df %>% filter(origin == "AngloSaxon")
+tmp <- df %>% filter(origin == "French")
+df_train <- rbind(df_train, tmp)
+tmp <- df %>% filter(origin == "German")
+df_train <- rbind(df_train, tmp)
+tmp <- df %>% filter(origin == "HispanicLatinAmerica")
+df_train <- rbind(df_train, tmp)
+tmp <- df %>% filter(origin == "India")
+df_train <- rbind(df_train, tmp)
+tmp <- df %>% filter(origin == "Italian")
+df_train <- rbind(df_train, tmp)
+tmp <- df %>% filter(origin == "Korea")
+df_train <- rbind(df_train, tmp)
+tmp <- df %>% filter(origin == "Japan")
+df_train <- rbind(df_train, tmp)
+
+tmp <- df %>% filter(origin == "MiddleEast")
+df_train <- rbind(df_train, tmp)
+tmp <- df %>% filter(origin == "Turkey")
+df_train <- rbind(df_train, tmp)
+tmp <- df %>% filter(origin == "Persian")
+df_train <- rbind(df_train, tmp)
+
+tmp <- df %>% filter(origin == "Scandinavian")
+df_train <- rbind(df_train, tmp)
+
+# tmp <- df %>% filter(origin == "China&SouthEastAsia")# %>% sample_n(4000)
+# df_train <- rbind(df_train, tmp)
+tmp <- df %>% filter(origin == "China")
+df_train <- rbind(df_train, tmp)
+tmp <- df %>% filter(origin == "SouthEastAsia")
+df_train <- rbind(df_train, tmp)
+tmp <- df %>% filter(origin == "Russian&EastEurope")
+df_train <- rbind(df_train, tmp)
+
+## summarize: --------------------------------------------
+df_train %>% group_by(origin) %>% summarise(count = n(),
+                                            share = count / nrow(df_train))
+df_train <- df_train[sample(nrow(df_train), nrow(df_train)), ]
+paste0("training sample ready: ", nrow(df_train), " observations for training.")
+
+#####################################################
+############ CREATE A CHARACTER DICTIONARY ##########
+#####################################################
+
+# double-check: search for special characters ----------------------------------
+special_chars <- stri_extract_all(str = df_train$full_name, regex = "[^a-z]")
+special_chars <- unlist(special_chars)
+special_chars <- unique(special_chars)
+if(special_chars == " "){print("no special chars left")}else{warning("Special characters in sample")}
+
+## choose character dictionary -------------------------------------------------
+char_dict <- NULL
+for(i in 1:length(df_train$full_name)){
+        chars <- unlist(str_extract_all(df_train$full_name[i], "[:print:]"))
+        chars <- unique(chars)
+        char_dict <- c(char_dict, chars[!chars %in% char_dict])
+}
+char_dict <- c(sort(char_dict)[-1], " ","END")
+if(length(letters %in% char_dict) < 26){warning("Not all standard letters are included in the vocabulary")}else{
+        print("All standard letters are included in the vocabulary")}
+n_chars <- length(char_dict)
+
+# choose sequence length -------------------------------------------------------
+hist(nchar(df_train$full_name), main = "", 
+     xlab = "Number of characters per name") # highlight name length distribution
+tmp <- nchar(df_train$full_name)
+seq_tresh <- 30
+paste0("truncating ", length(tmp[tmp>seq_tresh]), " (", round(100-length(tmp[tmp<=seq_tresh]) / nrow(df_train) *100, 2),
+       "%) names to ", seq_tresh, " characters")
+max_char <- seq_tresh
+
+#######################################
+######### CHARACTER ENCODING ##########
+#######################################
+
+x_dat <- encode_chars(names = df_train$full_name, 
+                      seq_max = max_char,
+                      char_dict = char_dict,
+                      n_chars = n_chars)
+
+paste("names are one-hot-encoded with shape: ", 
+      paste0("(", paste(dim(x_dat), collapse = ", "), ")")
+)
+
+####################################
+######### ORIGIN ENCODING ##########
+####################################
+
+y_dat <- as.factor(df_train$origin)
+y_classes <- data.frame(levels = levels(y_dat), 
+                        numbers = seq(length(unique(y_dat)))
+)
+levels(y_dat) <- y_classes$numbers
+y_dat <- as.numeric(as.character(y_dat))
+y_dat <- to_categorical(y_dat)
+y_dat <- y_dat[, -1]
+
+print("All names classified and encoded for training the model.")
