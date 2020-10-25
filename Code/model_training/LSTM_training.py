d72c81719dee3db0db7ffcfc231b17d54c8402a0
matthnig <matthias.niggli@unibas.ch>
2020-10-25 15:05:52 +0100

    WIP: - encoding function in python code;

--- Code/model_training/LSTM_training.py
+++ Code/model_training/LSTM_training.py
@@ -5,31 +5,54 @@
 # Date:           21.10.2020                                    #
 #################################################################
 
-# Import packages
+#### Import packages ---------------------------------------------------------
 import tensorflow as tf
 import numpy as np
 import pandas as pd
 import pyreadr
 import os
+import sys
 from matplotlib import pyplot as plt
-print("All libraries loaded.")
+print("All packages loaded.")
 
-# Set directory---------------------------------------------------------------
+#### Set directory-------------------------------------------------------------
 path = "C:/Users/Matthias/Documents/GithubRepos/inventor_migration"
 os.chdir(path)
 os.getcwd()
 print("Directories specified")
 
-## Load the data -------------------------------------------------------------
+#### Load the data ------------------------------------------------------------
 df_train = pd.read_csv("Data/training_data/df_train.csv")
-x_dat = pyreadr.read_r("Data/training_data/x_dat.rds")
-y_dat = pyreadr.read_r("Data/training_data/y_dat.rds")
 print("Data for training the model successfully loaded.")
 
-## construct class levels -----------------------------------------------------
+#### Encode data for training the model ---------------------------------------
+## Load the functions
+# os.chdir(path+"/Code/model_training")
+# from names_encoding_function import encode_chars
+# os.chdir(path)
+function_path = path+"/Code/model_training"
+if function_path not in sys.path:
+    sys.path.append(function_path)
+from names_encoding_function import encode_chars
 
-# y_classes <- data.frame(
-#         levels = levels(as.factor(df_train$origin)),
-#         numbers = seq(length(unique(df_train$origin)))
-#         )
-# print("Data for training the model successfully loaded.")
+## Specify parameters for encoding:
+CHAR_DICT = list([chr(i) for i in range(97,123)])+[" ", "END"]
+SEQ_MAX = 30
+N_CHARS = 28
+NAMES = df_train["full_name"]
+
+## encode names
+x_dat = encode_chars(names = ["matthias", "martina"], char_dict = CHAR_DICT,
+             seq_max = SEQ_MAX, n_chars = N_CHARS)
+
+## construct class levels
+y_classes = {"regions": sorted(list(df_train["origin"].unique())),
+             "numbers": [i +1 for i in range(len(df_train["origin"].unique()))]}
+
+y_classes = pd.DataFrame(y_classes)
+print("Data for training the model successfully transformed.")
+
+
+
+#### Define train and test set ------------------------------------------------
+
