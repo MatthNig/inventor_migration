50d897906ca3b62aac167bf4f2ac3b3e4d8f1193
MatthNig <matthias.niggli@unibas.ch>
2020-11-03 18:20:07 +0100

    WIP: > exploratory data analysis about the onshoring of high-impact patents.

--- Code/frontier_patents/patent_fwd_citations.R
+++ Code/frontier_patents/patent_fwd_citations.R
@@ -1,10 +1,10 @@
-#################################################################
-# Description:    Script to determine the number of patents'    #
-#                 forward citations. Classification of frontier #
-#                 patents based on these received citations.    #
-# Authors:        Matthias Niggli/CIEB UniBasel                 #
-# Date:           02.11.2020                                    #
-#################################################################
+####################################################################
+# Description:    Script to (1) determine the number of patents'   #
+#                 forward citations (2) Classification of frontier #
+#                 patents based on these received citations.       #
+# Authors:        Matthias Niggli/CIEB UniBasel                    #
+# Date:           02.11.2020                                       #
+####################################################################
 
 #######################################
 ## Load packages and set directories ##
@@ -29,17 +29,14 @@ if(substr(x = getwd(),
 #### Load patent information ---------------------------------------------------
 #cit_dat <- readRDS(paste0(mainDir1,  "/created data/", "forward_cit_dat.rds"))
 pat_dat <- readRDS(paste0(mainDir1,  "/created data/", "pat_dat.rds"))
-head(pat_dat)
 print("Data on patent citations loaded")
 
 #### Load company information --------------------------------------------------
 firm_dat <- readRDS(paste0(mainDir1,  "/created data/", "firm_reg.rds"))
-head(firm_dat)
 print("Firm data of patent ownership loaded")
 
 #### Load inventor information -------------------------------------------------
 inv_dat <- readRDS(paste0(mainDir1,  "/created data/", "inv_reg_CHcommute_adj.rds"))
-head(inv_dat)
 print("Inventor data of patents loaded")
 
 #################################
@@ -47,93 +44,131 @@ print("Inventor data of patents loaded")
 #################################
 
 #### Patent information: -------------------------------------------------------
-# Sometimes there several equivalent patents per p_key. In these cases, use 
-# USPTO granted patents with the most 5-year citations as the reference patent.
-tmp <- setDT(pat_dat)
-keep_idx <- tmp[order(pat_off, fwd_cits5), .I[.N], keyby = p_key]$V1 # order by patent office and citations and choose the last obs per p_key
-pat_dat <- pat_dat[keep_idx, ]
-tmp <- NULL
-paste("Obtained patents information of", nrow(pat_dat), "different patents cleaned for equivalents.")
 
-#### Inventor information: -------------------------------------------------------
-# Sometimes there are several equivalent patents per p_key and inventor. In these cases, only use 
-# one patent per p_key
+# Problem:
+# Sometimes there several equivalent patents per p_key. E.g. the same invention 
+# is protected by a US and European patent. In these cases, use the USPTO patents 
+# with the most 5-year citations as the reference patent for this p_key.
+
+# order by patent office and citations and choose the last obs per p_key:
+tmp <- setDT(pat_dat)
+keep_idx <- tmp[order(pat_off, fwd_cits5), .I[.N], keyby = p_key]$V1 
+pat_dat <- pat_dat[keep_idx, ]
+paste("Obtained patents information of", nrow(pat_dat), "different patents cleaned for equivalents.")
+tmp <- NULL
+keep_idx <- NULL
+
+#### Inventor information: -----------------------------------------------------
+
+# Problem:
+# Sometimes there several equivalent patents per p_key and inventor. 
+# E.g. the same invention is protected by a US and European patent. 
+# To omit these duplicated patent-inventor pairs, only take one combination per p_key
 inv_dat <- inv_dat %>% distinct(p_key, name, .keep_all = TRUE) %>% as.data.frame()
 
 # for every inventor_name / p_key combination assign the region, lat and long information to all patents
 # ....
 
-# function(df){
-# 
-#         # identify patents with missing information and subset to these observations 
-#         NA_keys <- which(is.na(df$regio_inv))
-#         tmp0 <- df[NA_keys, ]
-#         tmp0 <- select(tmp0, - Up_reg_code, -regio_inv, -regio_pat)
-#         
-#         # search for the p_keys and inventors with region information in other patents
-#         tmp1 <- df %>% select(p_key, name, Up_reg_code, regio_inv, regio_pat) %>% 
-#                 filter(name %in% tmp0$name & p_key %in% tmp0$p_key & is.na(regio_inv) == FALSE)
-#         
-#         # join information from other patents to those with missing information
-#         tmp0 <- left_join(x = tmp0, y = tmp1, by = c("p_key", "name"))
-#         tmp0 <- tmp0[is.na(tmp0$regio_pat) == FALSE, ]
-#         
-#         }
+#### firm information -----------------------------------------------------
 
-
-#### firm information
-# Sometimes there several equivalent patents per p_key and firm. In these cases, only use 
-# one patent per p_key
+# Problem:
+# Sometimes there several equivalent patents per p_key and firm 
+# E.g. the same invention is protected by a US and European patent. 
+# To omit these duplicated patent-firm pairs, only take one combination per p_key
 firm_dat <- firm_dat %>% distinct(p_key, organization, .keep_all = TRUE) %>% as.data.frame()
+
+print("All data processing completed")
 
 #################################
 ####### Data merging ############
 #################################
 
-#### Assign firms to patent information data -----------------------------------
+#### Combine information on firms with patent characteristics ------------------
 VARS <- colnames(firm_dat)[!names(firm_dat) %in% names(pat_dat)]
 tmp <- firm_dat[, c("p_key", VARS)]
 colnames(tmp)[-c(1:2)] <- paste0(colnames(tmp)[-c(1:2)], "_firm")
-df <- full_join(pat_dat, tmp, by = "p_key")
+df <- merge(setDT(pat_dat, key = "p_key"), setDT(tmp, key = "p_key"), by = "p_key", all = TRUE) # merge.data.table
 
-#### Assign inventors to patent and firm information ---------------------------
+# Alternative attempts:
+# df0 <- full_join(pat_dat, tmp, by = "p_key") # dplyr
+# df1 <- setDT(pat_dat, key = "p_key")[setDT(tmp, key = "p_key")] # data.table right join
+
+#### Combine information on inventors with patent and firm characteristic-------
 VARS <- colnames(inv_dat)[!names(inv_dat) %in% names(df)]
 VARS <- VARS[!VARS %in% c("ipc_main", "pat_off_name")]
 tmp <- inv_dat[, c("p_key", VARS)]
 change_names <- colnames(tmp)[colnames(tmp) %in% c("Up_reg_code", "lat", "lng")]
 change_names <- which(colnames(tmp) %in% change_names)
 colnames(tmp)[change_names] <- paste0(colnames(tmp)[change_names], "_inv")
-df <- full_join(df, tmp, by = "p_key")
-df[1234:1244, ]
+df <- merge(setDT(df, key = "p_key"), setDT(tmp, key = "p_key"), by = "p_key", all = TRUE) # merge.data.table
 
-#################################
-####### Exploratory analysis ####
-#################################
+# Alternative attempts:
+#df <- full_join(df, tmp, by = "p_key") # dplyr
+
+print("Patent characteristics combined with firm and inventor information.")
+
+##########################
+####### Data analysis ####
+##########################
+
+# define a function which calculates the share of onshored high-impact patents
+# for a specific country and high-impact measure.
+
+onshoring_fun_country <- function(df, onshoring_country = "US", world_class_indicator = "world_class_90"){
+        
+        # subset to firms that are not from the onshoring_country and to high-impact patents of this country
+        tmp <- df %>% filter(country_firm != onshoring_country)
+        tmp <- tmp %>% filter_at(vars(starts_with(world_class_indicator)), any_vars(. == 1))
+        # tmp <- df %>% filter(country_firm != "US" & world_class_90 == 1) # old without function
+        
+        # Step 1: For this country, calculate the number of patents by firm and year
+        total_pat_firms <- setDT(tmp)[, .(total_patents = uniqueN(p_key)), by = .(organization, p_year)]
+        total_pat_firms <- setDT(total_pat_firms, key = c("organization", "p_year"))
+
+        # Step 2: For this country, calculate the number of patents with at least 
+        #         one domestic inventor by firm and year
+        total_pat_onshored <- setDT(tmp)[ctry_inv == onshoring_country, 
+                                   .(total_patents_onshored = uniqueN(p_key)), 
+                                   by = .(organization, p_year)]
+        total_pat_onshored <- setDT(total_pat_onshored, key = c("organization", "p_year"))
+        
+        # Step 3: Combine these two measures and calculate the share
+        tmp <- total_pat_onshored[total_pat_firms]
+        tmp$total_patents_onshored <- unlist(sapply(tmp$total_patents_onshored, 
+                                                    function(x)ifelse(is.na(x) == TRUE, 0, x)))
+        
+        # Step 4: calculate the overall share of onshored patents per country and year:
+        tmp <- tmp %>% group_by(p_year) %>%
+        summarise(total_patents_onshored = sum(total_patents_onshored),
+                  total_patents = sum(total_patents)) %>%
+        mutate(share_onshored = total_patents_onshored / total_patents)
+        
+        # Step 5: subset to 2015
+        tmp <- filter(tmp, p_year <= 2015)
+        
+        return(tmp)
+}
+
+# I could do the same thing for regions or tech-fields
+
+#### Get the data for specific countries ---------------------------------------
+countries <- c("US", "DE", "GB", "FR", "CH", "CN", "JP")
+plot_dat <- lapply(countries,
+                   function(x)onshoring_fun_country(df = df, 
+                                                    onshoring_country = x, 
+                                                    world_class_indicator = "world_class_90")
+                   )
+for(i in 1:length(countries)){plot_dat[[i]]$country <- countries[i]}
+plot_dat <- bind_rows(plot_dat)
+
+#### Plot the data for specific countries: -------------------------------------
+ggplot(plot_dat, aes(x = p_year, y = share_onshored, color = country))+
+        geom_line()+geom_point()+ylim(0, 0.15)+
+        labs(title = " R&D Onshoring",
+             subtitle =  paste(" Share of High-impact patents (Top 10%) from foreign firms \n with at least one domestic inventor by country"),
+             x = "Year", y = "Share of foreign owned patents with domestic inventors")
 
 
-## subset to non-U.S. firms & frontier patents
-tmp <- df %>% filter(country_firm != "US" & world_class_90 == 1)
-
-## for every firm calculate the share of frontier patents with at least 1 inventor from the U.S. per year
-# (1)
-# => group_by firm and p_year
-# => calculate number of distinct p_keys (= number of patents)
-tmp <- tmp %>% group_by(organization, p_year) %>%
-        distinct(p_key) %>% summarise(count = n())
-tmp[tmp$organization == "saint gobain abrasifs", ]
-
-# (2)
-# => group_by firm and p_year
-# => count number of patents with ctry_inv == "US"
-
-# (3)
-# => calculate share
-tmp <- filter(df, organization == "saint gobain abrasifs")
-
-## for every p_year, calculate the share of world class patents developed in the U.S.
-# => is this share increasing?
-# => what about specific tech_fields?
-# is it different for frontier and non-frontier firms?
 
 rm(list=ls())
 
